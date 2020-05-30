ba2fa07afab232a80ed944308617e54dd09651c7
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-05-30 22:40:38 +0200

    New structure

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Phaser from './lib/phaser.js'
+import Game from './scenes/Game.js'
+
+export default new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 480,
+    height: 640
+})
---
+++ src/scenes/Game.js
@@ -0,0 +1,11 @@
+import Phaser from '../lib/phaser.js'
+export default class Game extends Phaser.Scene {
+    constructor() {
+        super('game')
+    }
+    preload() {
+    }
+
+    create() {
+    }
+}
