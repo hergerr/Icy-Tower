97495a0ab9fa362ca8466a7543293b11c0849e5d
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-05-30 23:42:40 +0200

    Added platforms group

--- src/main.js
+++ src/main.js
@@ -4,5 +4,15 @@ import Game from './scenes/Game.js'
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 480,
-    height: 640
+    height: 640,
+    scene: Game,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 200
+            },
+            debug: true
+        }
+    }
 })
