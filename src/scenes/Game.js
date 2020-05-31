2a059cbc4cac230ed2b6cee308b52c4a01a7f6f0
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-05-31 15:04:42 +0200

    Collecting carrots works

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -67,7 +67,16 @@ export default class Game extends Phaser.Scene {
         });
 
         // kolizja platform i marchewek
-        this.physics.add.collider(this.platforms, this.carrots)
+        this.physics.add.collider(this.platforms, this.carrots);
+
+        // logika zbierania (kolejnosc: obiekty, callback, kolejny callback, kontekst)
+        this.physics.add.overlap(
+            this.player,
+            this.carrots,
+            this.handleCollectCarrot, // called on overlap
+            undefined,
+            this
+        )
     }
 
     update(t, dt) {
@@ -134,5 +143,17 @@ export default class Game extends Phaser.Scene {
         carrot.body.setSize(carrot.width, carrot.height);
 
         return carrot
+    }
+
+    /**
+    * @param {Phaser.Physics.Arcade.Sprite} player
+    * @param {Carrot} carrot
+    */
+    handleCollectCarrot(player, carrot) {
+        // hide from display
+        this.carrots.killAndHide(carrot)
+
+        // disable from physics world
+        this.physics.world.disableBody(carrot.body)
     }
 }
